2c8d9732129252b8ffeef37f7f55ef600e36fbe6
fryciarz7 <fryciarz7@gmail.com>
2021-09-27 11:34:53 +0200

    created cmd command creation method

--- Symbolic Link Creator/AppLogic/CmdLogic.cs
+++ Symbolic Link Creator/AppLogic/CmdLogic.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Symbolic_Link_Creator.AppLogic
 {
-    class CmdLogic
+    public class CmdLogic
     {
+        public CmdLogic()
+        {
+        }
+
+        public string GenerateCMDCommand(string radio, string toBeSelect, string newFol, string sourceName)
+        {
+            //create command
+            string strCmd = string.Format("/C mklink {0} \"{2}\\{3}\" \"{1}\"", radio, toBeSelect, newFol, sourceName);
+
+            return string.Empty;
+        }
     }
 }
