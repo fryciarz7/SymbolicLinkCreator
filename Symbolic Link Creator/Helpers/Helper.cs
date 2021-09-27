5ceef219cc5d4fae4478d2718bb2e5786a5a7aad
fryciarz7 <fryciarz7@gmail.com>
2021-09-27 11:26:55 +0200

    moved Helper class to new file

--- Symbolic Link Creator/Helpers/Helper.cs
+++ Symbolic Link Creator/Helpers/Helper.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Symbolic_Link_Creator.Helpers
 {
-    class Helper
+    public static class Helper
     {
+        /// <summary>
+        /// Opens FolderBrowserDialog and returns selected path of selected folder.
+        /// </summary>
+        /// <returns>String FolderBrowserDialog.SelectedPath</returns>
+        public static string SetTextBoxText()
+        {
+            string tbtext = string.Empty;
+            FolderBrowserDialog fbd;
+            try
+            {
+                using (fbd = new FolderBrowserDialog())
+                {
+                    //open FolderBrowserDialog
+                    DialogResult result = fbd.ShowDialog();
+                    //check if there is folder selected
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        return fbd.SelectedPath;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException("Source: " + ex.Source + "\nError: " + ex.Message + "\nInnerEx: " + ((ex.InnerException == null) ? "null" : ex.InnerException.Message));
+            }
+            return tbtext;
+        }
     }
 }
