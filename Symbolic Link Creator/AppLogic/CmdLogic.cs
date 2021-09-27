526282bc658d117c4b1523863b1b965bef76fbad
fryciarz7 <fryciarz7@gmail.com>
2021-09-27 11:28:52 +0200

    added CmdLogic class

---
+++ Symbolic Link Creator/AppLogic/CmdLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symbolic_Link_Creator.AppLogic
+{
+    class CmdLogic
+    {
+    }
+}
